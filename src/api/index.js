a7e0db01b424f41d7e9bdeb6938bacfcc41d3e21
chensuya <sy1981427665@163.com>
2022-02-26 15:59:02 +0800

    home模块完成

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+import requests from './ajax';
+import mockRequests from './mockAjax'
+//TypeNav接口
+//发请求：axios发请求返回结果Promise对象
+export const reqCategoryList = () => {
+    return requests({ url: '/product/getBaseCategoryList', method: 'get' });
+}
+
+//获取banner
+export const reqGetBannerList = () => mockRequests.get('/banner');
+export const reqGetFloorList = () => mockRequests.get('/floor');
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <typeNav></typeNav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
